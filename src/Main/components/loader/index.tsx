3bf8647d8a30209ecf90c74b2870dd0349b750ca
RUI <cazofu2802@gmail.com>
2023-06-23 01:42:41 +0800

    [ryan update] fix loader bug

--- src/Main/components/loader/index.tsx
+++ src/Main/components/loader/index.tsx
@@ -4,24 +4,27 @@ import Lenis from '@studio-freight/lenis'
 import { useEffect } from "react";
 
 const Loader: React.FC = () => {
-    const lenis = new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        orientation: 'vertical', // vertical, horizontal
-        gestureOrientation: 'vertical', // vertical, horizontal, both
-        smoothWheel: true,
-        wheelMultiplier: 1,
-        smoothTouch: false,
-        touchMultiplier: 2,
-        infinite: false,
-    })
-    lenis.stop()
-    function raf(time: number) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-    }
-    requestAnimationFrame(raf)
+   
     useEffect(() => {
+
+        const lenis = new Lenis({
+            duration: 1.2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            orientation: 'vertical', // vertical, horizontal
+            gestureOrientation: 'vertical', // vertical, horizontal, both
+            smoothWheel: true,
+            wheelMultiplier: 1,
+            smoothTouch: false,
+            touchMultiplier: 2,
+            infinite: false,
+        })
+        lenis.stop()
+        function raf(time: number) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+        requestAnimationFrame(raf)
+        
         //loader
         gsap.to('.loader', {
             x: '100%', duration: 0.8, ease: 'power4.inOut', delay: 1.5, onComplete: () => {
