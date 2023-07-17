687b82d62b1ef8553164d1d0503a6d34ee71572f
sean <sean920102@gmail.com>
2023-07-17 23:04:56 +0800

    [Sean feat] add CardFiddle component

---
+++ src/Sean/components/cardFiddle/index.tsx
@@ -0,0 +1,9 @@
+import './style.scss';
+export const CardFiddle = () => {
+  return (
+    <div className="cardFiddle">
+      <div className="card__side front">🂭</div>
+      <div className="card__side back">🃞</div>
+    </div>
+  );
+};
--- src/Sean/pages/testPage/index.tsx
+++ src/Sean/pages/testPage/index.tsx
@@ -1,6 +1,7 @@
 import './style.scss'
-import Tabs from '../main-display-page/components/tabs'
-import {tabs} from '../main-display-page/config';
+// import Tabs from '../main-display-page/components/tabs'
+// import {tabs} from '../main-display-page/config';
+import { CardFiddle } from '@/Sean/components/cardFiddle'
 const TestingPage: React.FC = () => {
   return(
     <div className="testWrapper">
@@ -8,7 +9,8 @@ const TestingPage: React.FC = () => {
         <span className="aboutInfo-title b_gray">Testing</span>
       </div>
       <div className="">
-        <Tabs tabProps={tabs}/>
+        {/* <Tabs tabProps={tabs}/> */}
+        <CardFiddle></CardFiddle>
       </div>
     </div>
   ) 
